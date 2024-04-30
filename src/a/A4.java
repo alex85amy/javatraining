2c60fd1d09ffe65022fac030c72a39da62e62f52
alex85amy <alex85amy@gmail.com>
2024-04-30 17:51:07 +0800

    update

--- src/a/A4.java
+++ src/a/A4.java
@@ -1,11 +1,37 @@
 package a;
 
-public class A4 {
-//    A.請寫出一個程式，接受命令列的一個參數做為properties主檔名，
-//    以Properties API開啟該檔後，使用log4j方法印出該檔內所有Name及Value。
-//    (請自行先製作一份properties檔，裡面內含key及value需2至N組，其中的Value須包含中文，
-//    印出來也必須是中文字，提示JAVA IO中可指定檔案編碼格式。)
-//    B.程式讀取之properties檔，以Writer或其子類別的方法寫入另一個新文字檔內，
-//    新文字檔的檔名是採用命令列提供的第二個參數，且讓每個Name, Value成為一行，格式為[Name, Value]。
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.*;
+import java.util.Properties;
+
+public class A4 {
+    public static void main(String[] args) throws IOException {
+        Logger logger = LogManager.getLogger();
+        if (args.length != 2) {
+            System.out.println("請輸入正確參數");
+        } else {
+            InputStream inputStream = ClassLoader.getSystemResourceAsStream(args[0] + ".properties");
+            Properties properties = new Properties();
+            properties.load(new InputStreamReader(inputStream, "utf-8"));
+
+            File file = new File(args[1]);
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+
+            for (String key : properties.stringPropertyNames()) {
+                logger.info(key + " : " + properties.getProperty(key));
+                writer.write("[" + key + ", " + properties.getProperty(key) + "]\n");
+            }
+
+            inputStream.close();
+            writer.close();
+        }
+    }
 }
+
+
+
+
+
